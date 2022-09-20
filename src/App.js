b16e545574ea2de0e6e8d1996cdc35ab9ea5702a
hani2057 <hanikim2057@gmail.com>
2022-09-20 17:51:19 +0900

    0920

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import NewsList from './components/NewsList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <NewsList />;
+};
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   // 아래와 같은 기능을 함
+//   // const onClick () => {
+//   //   axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+//   //     setData(response.data);
+//   //   });
+//   // };
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get(
+//         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=c88685561b9a45de81f56862936c68a4',
+//       );
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 
 export default App;
